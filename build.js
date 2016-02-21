43da7564287dd8b57a0a553e049dc134d1ee8316
Sergey Dyachenko <yourselfwar@bk.ru>
2016-02-21 20:01:28 +0200

    compile

---
+++ build.js
@@ -0,0 +1,13 @@
+/**
+ * Example from http://www.svlada.com/require-js-optimization-part2/
+ */
+({
+    appDir: './',
+    baseUrl: './js',
+    dir: './dist',
+    modules: [
+        {
+            name: 'main'
+        }
+    ]
+})
