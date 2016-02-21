765620f136fd821a85f6af71638d80d3301c9c18
Sergey Dyachenko <yourselfwar@bk.ru>
2016-02-21 10:26:39 +0200

    start

---
+++ js/Views/ListView.js
@@ -0,0 +1,17 @@
+define(function(){
+	function render(parameters){
+		var appDiv = document.getElementById('app');
+		var users = parameters;
+		var html = '<ul>';
+
+		for (var i = 0, len = users.length; i < len; i++){
+			html += '<li>' + users[i].name + '</li>';
+		}
+		html += '</ul>';
+		appDiv.innerHTML = html;
+	}
+
+	return {
+		render:render
+	};
+});
